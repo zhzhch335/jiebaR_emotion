d1ccf70b8e281e5195961c2a232dce270bcb65b7
zhzhch335 <357611628@qq.com>
2017-05-15 18:47:32 +0800

    加入词典，修改控制台测试输出语句，修改readme文件加入计算公式

--- main.R
+++ main.R
@@ -1,12 +1,12 @@
-setwd("G:/workcloud/bjtudrive/Rworkspace/emotion analysis")
+setwd("G:/workcloud/bjtudrive/Rworkspace/jiebaR_emotion")#！！！这只是我自己的根目录请记得按照自己的目录修改
 library(jiebaRD)
-library(jiebaR)
-jieba<-worker(type="tag",user="G:\\R\\R-3.3.3\\library\\jiebaRD\\dict\\emotioncal.dict.utf8",symbol = TRUE)
+library(jiebaR)#引入两个jieba包
+jieba<-worker(type="tag",user="dict/emotioncal.dict.utf8",symbol = TRUE)#这里的user属性填写情感计算词典的路径，情感词典格式见例子
+#定义分析函数
 ecal<-function(str){
-  result<-jieba<="#2017两会#他被称作“最牛中学校长”，他快人快语，流传很多经典语录。“我们都没有把学生当成一个活生生的人”“凡是把孩子放在第一位的，多半是悲剧”…今晚8点，@人民日报 对话全国政协委员、北京四中校长刘长铭，敬请关注！http://t.cn/RijCRoW <U+200B><U+200B><U+200B>"
+  result<-jieba<=str#将字符串进行分词和标记处理
   winfront<-1L#上一个窗口边界索引（可能是一个行首，一个情感词或者一个标点符号）
   count<-1#定义窗口的索引（便于句子结束时候的累加）
-  #value<--1#定义情感的基本方向（本例中是消极统计）
   winvalue<-c()#存放窗口积极感情倾向值的数组
   posvalue<-c()#存放单句积极情感倾向总值的数组
   negvalue<-c()#存放单句消极情感倾向总值的数组
@@ -17,11 +17,8 @@ ecal<-function(str){
   rheflag<-FALSE
   for (i in 1:length(result)){
     #开始查找情感倾向词
-    if(names(result[i])=="positive"){
-      cat("find a positive word",result[i],".")
-      print(" ")
-      cat("这时候的pvalue nvalue值是",pvalue,nvalue,".")
-      print(" ")
+    if(names(result[i])=="positive"){      
+      cat("发现积极感情倾向词：",result[i],"\n")      
       #判断是否第一位
       if(i==1)
         winvalue[count]<-1
@@ -40,18 +37,14 @@ ecal<-function(str){
             rheflag<-TRUE
         }
       }
-      cat("经过计算这个就窗口值为",winvalue[count])
-      print(" ")
+      cat("经过计算这个滑动窗口值为：",winvalue[count],"\n")      
       #窗口计算值完毕
       count=count+1
       #更新上一个窗口边界
       winfront<-i+1
     }
     else if(names(result[i])=="negative"){
-      cat("find a negative word",result[i])
-      print(" ")
-      cat("这时候的pvalue nvalue值是",pvalue,nvalue,".")
-      print(" ")
+      cat("发现消极情感倾向词：",result[i],"\n")      
       if(i==1)
         winvalue[count]<--1
       else{
@@ -69,8 +62,7 @@ ecal<-function(str){
             rheflag<-TRUE
         }
       }
-      cat("经过计算这个就窗口值为",winvalue[count])
-      print(" ")
+      cat("第",count,"个窗口计算结束，这个就窗口情感值为：",winvalue[count],"\n")      
       #窗口计算值完毕
       count=count+1
       #更新上一个窗口边界
@@ -78,10 +70,7 @@ ecal<-function(str){
     }
     #开始查找句子边界符号或字符串结尾
     if(result[i]%in%c("?",".","!","？","。","！")|is.na(result[i+1])){
-      cat("发现结束符，句子结束，此时窗口值为",winvalue)
-      print(" ")
-      cat("这时候的pvalue nvalue值是",pvalue,nvalue,".")
-      print(" ")
+      cat("发现结束符，句子结束，此时各个窗口值为：",winvalue,"\n")                  
       if(!is.null(winvalue)){
         posvalue[ppcount]<-0
         negvalue[nncount]<-0
@@ -94,18 +83,17 @@ ecal<-function(str){
             negvalue[nncount]<-negvalue[nncount]+winvalue[k]
           }
         }
+        cat("这时候的积极情感值、消极情感值值分别是：",pvalue,nvalue,"\n")
         if(result[i]%in%c("!","！")){
           posvalue[ppcount]=posvalue[ppcount]*2
           negvalue[nncount]=negvalue[nncount]*2
-          cat("处理感叹句，处理后pvalue和nvalue为",pvalue,nvalue)
-          print(" ")
+          cat("处理感叹句，处理后积极情感值和消极情感值分别为：",pvalue,nvalue,"\n")          
         }
         else if(result[i]%in%c("?","？") & rheflag==TRUE ){
           temp<-negvalue[nncount]
           negvalue[nncount]=posvalue[nncount]*(-2)
           posvalue[ppcount]=temp*(-2)
-          cat("处理反问句，处理后pvalue和nvalue为",pvalue,nvalue)
-          print(" ")
+          cat("处理反问句，处理后积极情感值和消极情感值分别为：",pvalue,nvalue,"\n")          
         }
         #更新上一个窗口边界
         winfront<-i+1
@@ -120,15 +108,5 @@ ecal<-function(str){
       }
     }
   }
-
-  # pvalue<-0L #最终积极情感值
-  # nvalue<-0L #最终消极情感值
-  # #输出各项指标
-  # for (i in 1:ppcount-1) {
-  #   pvalue<-pvalue+posvalue[i]
-  # }
-  # for (i in 1:nncount-1) {
-  #   nvalue<-nvalue+negvalue[i]
-  # }
-
+  return(c(pvalue,nvalue))
 }
